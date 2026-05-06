18a72074273456fa05c647c3bd71d989176dc756
Vaibhav Saini <sainivaibhav1409@gmail.com>
2026-05-06 21:52:05 +0530

    Added js file

---
+++ Task - 1/script.js
@@ -0,0 +1,17 @@
+function showInfo(city) {
+
+  if (city === 'Paris') {
+    alert('Paris, France\n\nBest time to visit: April to June\nCurrency: Euro\nLanguage: French');
+  } 
+  else if (city === 'Tokyo') {
+    alert('Tokyo, Japan\n\nBest time to visit: March to May\nCurrency: Japanese Yen\nLanguage: Japanese');
+  } 
+  else if (city === 'Sydney') {
+    alert('Sydney, Australia\n\nBest time to visit: September to November\nCurrency: Australian Dollar\nLanguage: English');
+  }
+  
+}
+
+function sayHello() {
+  alert('Hello! Thanks for visiting my travel blog.\nHope it inspires you to explore the world!');
+}
